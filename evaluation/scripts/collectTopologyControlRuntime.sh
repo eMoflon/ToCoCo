4cbe6daa579820bdef6a950bb0417d37936c50f5
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-11-02 07:37:05 +0100

    Improve runtime evaluation script

--- evaluation/scripts/collectTopologyControlRuntime.sh
+++ evaluation/scripts/collectTopologyControlRuntime.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# This script produces a CSV that summarizes the topology control runtime per mote
+#
+# Usage: Pass the serial.csv file to analyze as first parameter
+#
+# Author: Roland Kluge
+#
+
+# We use the sanitized version of the serial output file
+inputFile="${1%.*}_excel.csv"
+
 [ -f $1 ] || {
 echo "Input file $inputFile does not exist"
 exit 1
 }
 
-inputFile="${1%.*}_excel.csv"
 
 algorithm=$(grep "topologycontrol: " $inputFile | head -1 | sed -r "s/^.*topologycontrol: (.*)'.*/\1/g")
 outputFolder="$(dirname $inputFile)/output"
 mkdir -p $outputFolder
-outputFile="$outputFolder/topologyControlRuntime.csv"
+outputFile="$outputFolder/runtime_${algorithm}.csv"
 
 echo "00algo;nodeId;runtimeMillis" > $outputFile
-grep -a "TIME:" $inputFile | cut -d";" -f2,3 | sed -r 's/".*: TIME: ([[:digit:]]+),"/\1/g;s/^([[:digit:]]);/0\1;/g' | sort >> $outputFile
+grep -a "TIME:" $inputFile | cut -d";" -f2,3 | /usr/bin/sort --numeric-sort | sed -r 's/"\[topologycontrol\]: TIME: ([[:digit:]]+) .*\"/\1/g' >> $outputFile
 sed -i 's/00algo/algo/' $outputFile
 sed -i "2,\$s|^|${algorithm};|" $outputFile
